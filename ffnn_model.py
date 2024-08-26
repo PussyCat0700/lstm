080885fceff5518fd01bbdf0dd1bef306687b4cd
刘一凡 <1729372667@qq.com>
2024-08-26 21:17:12 +0800

    (feat) update ffnn to remove temporal mapping

--- ffnn_model.py
+++ ffnn_model.py
@@ -5,23 +5,22 @@ import torch.nn.functional as F
 nwp_input_size = 16  # NWP data has 16 features
 
 class WindPowerFFNN(nn.Module):
-    def __init__(self, input_dim=nwp_input_size, output_dim=1):
+    def __init__(self, input_dim=nwp_input_size*48):
         super(WindPowerFFNN, self).__init__()
-        self.nwp_mlp = nn.Linear(48, 96)
-        self.fc = nn.Linear(input_dim, output_dim)
+        self.fc = nn.Linear(input_dim, 96)
         self.sigmoid = nn.Sigmoid()
         # Batch Norm Layer, without which the model fails to perform
         self.batch_norm = nn.BatchNorm1d(nwp_input_size)
 
 
     def forward(self, nwp_data):
-        nwp_data = self.nwp_mlp(nwp_data.transpose(-1, -2)).transpose(-1, -2)  # [B, 96, nwp_input_size]
         # Apply batch normalization across the time dimension
         nwp_data = nwp_data.permute(0, 2, 1)  # Switch batch and feature dimensions
         nwp_data = self.batch_norm(nwp_data)
-        nwp_data = nwp_data.permute(0, 2, 1)  # Switch them back
-        output = self.fc(nwp_data)  # [B, 96, output_dim]
-        output = self.sigmoid(output).squeeze(-1)
+        nwp_data = nwp_data.permute(0, 2, 1)  # Switch them back to [B, 96, 16]
+        x = nwp_data.reshape(nwp_data.shape[0], -1)
+        output = self.fc(x)  # [B, 96]
+        output = self.sigmoid(output)
         return output
 
 
