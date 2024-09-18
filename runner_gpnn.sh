c297abf1560f73ff86aec8108dab72389a633d89
刘一凡 <1729372667@qq.com>
2024-09-18 21:44:48 +0800

    (feat) scripts

---
+++ runner_gpnn.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --account=yfliu3
+#SBATCH --job-name=gpnn
+#SBATCH --partition=RTX4090,RTX3090,ADA6000 # 用sinfo命令可以看到所有队列
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1 # 若多卡或多进程，请调整此参数
+#SBATCH --cpus-per-task=16  # 每个进程的CPU数量
+#SBATCH --gres=gpu:1        # 若使用2块卡，则gres=gpu:2
+#SBATCH --output=./logs/gpnn.out
+#SBATCH --error=./logs/gpnn.err
+#SBATCH --time=50:00:00
+
+for i in {0..183}
+do
+    # 替换 %d 为当前的数字 i
+    ckpt_dir="/data1/yfliu/windpower_baseline/gpnn_hourly/gpnn_$i"
+    # 执行命令，传递参数并替换 %d
+    python train.py 2 --plant_number $i --checkpoint_dir $ckpt_dir --num_epochs 1000
+done
+python gather_results.py 2
