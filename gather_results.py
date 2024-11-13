49344c7ebe34b2c7c2d3b4b429031bb2d34cd349
刘一凡 <1729372667@qq.com>
2024-11-13 14:57:19 +0800

    (feat) filtered stations

--- gather_results.py
+++ gather_results.py
@@ -11,31 +11,39 @@ args = parser.parse_args()
 
 ckpt_dir = results_save_path
 if args.model_type_number == 0:
-    subdir = "lstm_hourly"
+    subdir = "lstm"
 elif args.model_type_number == 1:
-    subdir = "ffnn_hourly"
+    subdir = "ffnn"
 elif args.model_type_number == 2:
-    subdir = "gpnn_hourly"
+    subdir = "gpnn"
 elif args.model_type_number == 3:
     subdir = "xgboost"
 elif args.model_type_number == 4:
     subdir = "gpcf"
 ckpt_dir = os.path.join(ckpt_dir, subdir)
 output_file = f"averaged_metrics_{subdir}.csv"
+filtered_record_file = f"filtered_stations_{subdir}.txt"
 
 # Initialize an empty DataFrame to store metrics from all files
 all_metrics = []
-
+all_filtered = []
 # Loop through all subdirectories in ckpt_dir
 for subdir in os.listdir(ckpt_dir):
     subdir_path = os.path.join(ckpt_dir, subdir)
     metrics_file = os.path.join(subdir_path, "metrics.csv")
     
+    station_number = subdir.split('_')[-1]
     # Check if metrics.csv exists in the current subdirectory
     if os.path.isfile(metrics_file):
         # Read the metrics.csv and append to the list
         df = pd.read_csv(metrics_file)
+        df['station'] = station_number
         all_metrics.append(df)
+    else:
+        all_filtered.append(station_number)
+
+with open(filtered_record_file, 'w') as f:
+    f.writelines([x+'\n' for x in all_filtered])
 
 # Combine all metrics into a single DataFrame
 if all_metrics:
