9562dfc495ebcbc9cb329babec1d6495acd62dac
刘一凡 <1729372667@qq.com>
2024-11-19 20:47:36 +0800

    (feat) 6m

--- runner_ffnn.sh
+++ runner_ffnn.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
 #SBATCH --account=yfliu3
-#SBATCH --job-name=ffnn
+#SBATCH --job-name=6mffnn
 #SBATCH --partition=RTX4090,RTX3090,ADA6000 # 用sinfo命令可以看到所有队列
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1 # 若多卡或多进程，请调整此参数
 #SBATCH --cpus-per-task=16  # 每个进程的CPU数量
 #SBATCH --gres=gpu:1        # 若使用2块卡，则gres=gpu:2
-#SBATCH --output=./logs/ffnn.out
-#SBATCH --error=./logs/ffnn.err
+#SBATCH --output=./logs/6mffnn.out
+#SBATCH --error=./logs/6mffnn.err
 #SBATCH --time=7-00:00:00
 #SBATCH --mail-type=all
 #SBATCH --mail-user=1729372667@qq.com
 
-for i in {0..328}
+for i in {0..436}
 do
     # 替换 %d 为当前的数字 i
-    ckpt_dir="/data1/yfliu/solar_baseline/runs_ffnn/ffnn_$i"
+    ckpt_dir="/data1/yfliu/solar_baseline/ablation_ffnn/6m/ffnn_$i"
     # 执行命令，传递参数并替换 %d
-    python train.py 1 --plant_number $i --checkpoint_dir $ckpt_dir --num_epochs 1000
+    python train.py 1 --plant_number $i --checkpoint_dir $ckpt_dir --num_epochs 1000 --batch_size 1024
 done
-python gather_results.py 1
