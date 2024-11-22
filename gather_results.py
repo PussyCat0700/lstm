7c5fa185cda48dcf179ffd525addde0a73d65d04
刘一凡 <1729372667@qq.com>
2024-11-22 22:54:17 +0800

    (feat) zip

--- gather_results.py
+++ gather_results.py
@@ -1,6 +1,9 @@
 import argparse
 import os
 import pandas as pd
+import zipfile
+
+from tqdm import tqdm
 from paths import PLANTS, path_loader
 
 
@@ -9,6 +12,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("model")
 parser.add_argument("months")
 parser.add_argument("plantset", choices=PLANTS.keys())
+parser.add_argument("--zip", action='store_true')
 args = parser.parse_args()
 
 path_loader.init(args.months, args.plantset, 0)
@@ -18,6 +22,10 @@ df = pd.read_csv(info_csv_file)
 grouped = df.groupby('TYPE')
 save_path = os.path.join('./outputs', args.plantset, args.model, args.months)
 os.makedirs(save_path, exist_ok=True)
+
+output_zipfile = os.path.join(save_path, f"{args.plantset}_{args.model}_{args.months}.zip")
+if args.zip:
+    zipf = zipfile.ZipFile(output_zipfile, 'w', zipfile.ZIP_DEFLATED)
 # 对每个TYPE组进行处理
 for group_type, group_data in grouped:
     # 这里你可以对每个分组的数据进行操作
@@ -31,6 +39,7 @@ for group_type, group_data in grouped:
     all_filtered = []
     all_extreme_large = []
     # Loop through all subdirectories in ckpt_dir
+    pbar = tqdm(range(len(group_data)))
     for idx, row in group_data.iterrows():
         station_number = str(int(row['PLANT_NO']))
         station_path, _ = path_loader.get_run_path_status(args.model)
@@ -43,10 +52,19 @@ for group_type, group_data in grouped:
             df['station'] = station_number
             if df["rmse"][0] >= 0:
                 all_metrics.append(df)
+                if args.zip:
+                    file_paths = [os.path.join(station_path, x) for x in ['all_gts.npy', 'all_preds.npy']]
+                    for file_path in file_paths:
+                        if not os.path.isfile(file_path):
+                            continue
+                        relative_path = f'{station_number}/{os.path.basename(file_path)}'
+                        # 添加文件到 zip 包
+                        zipf.write(file_path, arcname=relative_path)
             else:
                 all_extreme_large.append(station_number)
         else:
             all_filtered.append(station_number)
+        pbar.update()
 
     with open(filtered_record_file, 'w') as f:
         f.writelines([x+'\n' for x in all_filtered])
@@ -71,3 +89,5 @@ for group_type, group_data in grouped:
         print(f"Averaged metrics saved in {output_file}")
     else:
         print("No metrics.csv files found.")
+if args.zip:
+    zipf.close()
