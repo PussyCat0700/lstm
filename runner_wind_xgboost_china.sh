10d2c4143d16bddcdf6ac08eb1bb9f94f7fc0898
刘一凡 <1729372667@qq.com>
2025-01-24 23:28:25 +0800

    (feat) replace xgboost with pb as pb runs too slowly

--- runner_wind_xgboost_china.sh
+++ runner_wind_xgboost_china.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 #SBATCH --account=yfliu3
-#SBATCH --job-name=xgboostpb_nmg_wnd
+#SBATCH --job-name=xgboost_china_wnd
 #SBATCH --time=00:15:00
 #SBATCH --partition=RTX3090,RTX4090,L40S,ADA6000
 #SBATCH --cpus-per-task=12  # 每个进程的CPU数量
-#SBATCH --array=0-201:10%1       # 任务ID范围
+#SBATCH --array=0-286:10%1       # 任务ID范围
 #SBATCH --mem=40GB
 #SBATCH --qos=ne_ablation
-#SBATCH --output=./logs/station_logs/nmg/xgboostpb_%A_%a.out
-#SBATCH --error=./logs/station_logs/nmg/xgboostpb_%A_%a.err
+#SBATCH --output=./logs/station_logs/china/xgboost_%A_%a.out
+#SBATCH --error=./logs/station_logs/china/xgboost_%A_%a.err
 #SBATCH --time=7-00:00:00
 
 # 获取当前任务ID
@@ -21,9 +21,9 @@ for i in $(seq 0 $((offset-1)))
 do
     plant_number=$((task_id + i))
     echo "Running task for plant_number: $plant_number"
-    logdir=/data1/yfliu/logs/solar/nmg/xgboostpb/${1}_${task_id}_${plant_number}
+    logdir=/data1/yfliu/logs/solar/china/xgboost/${1}_${task_id}_${plant_number}
     echo "exporting to" "$logdir/log.txt"
     mkdir -p $logdir
-    python sklearn_train.py 4 --plant_number $plant_number --months $1 --plant_set "nmg" --plant_type 0 --period 24 > "$logdir/log.txt" 2>&1 &
+    python sklearn_train.py 0 --plant_number $plant_number --months $1 --plant_set "china" --plant_type 0 --period 24 > "$logdir/log.txt" 2>&1 &
 done
 wait
